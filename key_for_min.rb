a4666dbf3261864dc9743be0506f5c5d238c60d7
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-23 06:19:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,9 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if value < lowest_value
         lowest_value = value
+        lowest_key = key
       end
     end
-    lowest_value
+    lowest_key
   end
 end
